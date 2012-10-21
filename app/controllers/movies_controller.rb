7dd94f6accd164cffb7bb48fe2bb9c23344f760e
saasbook <saasbook@saasbook.(none)>
2012-10-21 15:15:52 -0700

    HW2_Q2_and_Q3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,12 +7,38 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-    sort_symbol = session[:sort_by]
-    
-    unless sort_symbol.nil?
-      @movies = @movies.sort_by { |each| each.send(sort_symbol) }
+    #@movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = []
+
+    @movies = []
+    if params.key?(:ratings)
+      if params[:ratings].empty?
+        session.delete(:ratings)
+        # puts "params ratings empty"
+      else
+        session[:ratings] = params[:ratings].keys
+        #puts "params ratings not empty"
+      end
+    else
+      #puts "no params ratings"
     end
+
+    if session.key?(:ratings)
+      #params[:ratings].keys.each do |k|
+      session[:ratings].each do |k|
+      @movies = @movies + Movie.where("rating=?",k)
+      @selected_ratings = @selected_ratings + [k]
+    end
+    else
+      @movies = @movies + Movie.all
+    end
+
+    sort_symbol = session[:sort_by]
+    unless sort_symbol.nil?
+      @movies = @movies.sort_by { |m| m.send(sort_symbol) }
+    end
+
   end
 
   def sort_title
